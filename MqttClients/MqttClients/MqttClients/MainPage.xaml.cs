d4b573081b9bfb0182e22f88de5448bcde728079
Stathis Votsis <stathisvotsis@gmail.com>
2018-03-26 23:06:09 +0300

    hdhdh

--- MqttClients/MqttClients/MqttClients/MainPage.xaml.cs
+++ MqttClients/MqttClients/MqttClients/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace MqttClients
 {
@@ -13,13 +14,17 @@ namespace MqttClients
 		public MainPage()
 		{
 			InitializeComponent();
-            MqttClient mq1 = new MqttClient("farsala.ddns.net");
+            MqttClient mq1 = new MqttClient("192.168.1.2");
 
             try
             {
                 byte code = mq1.Connect(Guid.NewGuid().ToString(), "evotsis", "eystbots");
                 DisplayAlert("Alert", code.ToString(), "OK1");
-                
+                ushort msgId = mq1.Publish("test", // topic
+                              Encoding.UTF8.GetBytes("MyMessageBody"), // message body
+                              MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, // QoS level
+                              true);
+
             }
             catch (Exception)
             {
