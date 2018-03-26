d64fe26b0993ad3de7f53c6d5d3c8e36953e95e1
StathisVotsis <stathisvotsis@gmail.com>
2018-03-26 15:31:12 +0300

    hsdgshdg

---
+++ MqttClients/MqttClients/MqttClients/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace MqttClients
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            MqttClient mq1 = new MqttClient("farsala.ddns.net");
+
+            try
+            {
+                byte code = mq1.Connect(Guid.NewGuid().ToString(), "evotsis", "eystbots");
+                DisplayAlert("Alert", code.ToString(), "OK1");
+                
+            }
+            catch (Exception)
+            {
+                DisplayAlert("Alert", "You have been alerted", "OK2");
+            }
+		}
+	}
+}
